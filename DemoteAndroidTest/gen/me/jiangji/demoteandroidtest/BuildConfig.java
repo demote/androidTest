947171a5627d46199b3d5d5a43274cccbd7e4164
Jiang Ji <jiangji@ict.ac.cn>
2013-09-25 10:44:08 +0800

    add another implementation about spinner adapter

--- DemoteAndroidTest/gen/me/jiangji/demoteandroidtest/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package me.jiangji.demoteandroidtest;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- DemoteAndroidTest/src/me/jiangji/demoteandroidtest/SpinnerActivity.java
+++ DemoteAndroidTest/src/me/jiangji/demoteandroidtest/SpinnerActivity.java
@@ -13,7 +13,8 @@ import android.widget.TextView;
 
 public class SpinnerActivity extends Activity {
 
-	private static final String[] items = { "hello", "world", "Dont", "panic" };
+	private static final String[] items = { "hello", "world"};
+	private static final String[] items2 = {"Dont", "Panic"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +23,44 @@ public class SpinnerActivity extends Activity {
 		setContentView(R.layout.activity_spinner);
 
 		Spinner mySpinner = (Spinner) findViewById(R.id.simple_spinner);
-		// ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,
-		// R.layout.simple_list_item,items);
-		MySpinnerAdapter spinnerAdapter = new MySpinnerAdapter(this, R.layout.simple_list_item,
-						items);
+		MySpinnerAdapter spinnerAdapter = new MySpinnerAdapter(this, R.layout.simple_list_item, items);
+		
+		Spinner mySpinner2 = (Spinner) findViewById(R.id.simple_spinner2);
+		MySpinnerAdapter2 spinnerAdapter2 = new MySpinnerAdapter2(this, R.layout.simple_list_item);
+		spinnerAdapter2.addAll(items2);
 
 		mySpinner.setAdapter(spinnerAdapter);
+		mySpinner2.setAdapter(spinnerAdapter2);
 	}
 
 	private class MySpinnerAdapter extends ArrayAdapter<String> {
 
 		public MySpinnerAdapter(Context context, int textViewResourceId, String[] objects) {
 			super(context, textViewResourceId, objects);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView v = (TextView) super.getView(position, convertView, parent);
+			v.setTextColor(Color.BLUE);
+			return v;
+		}
+
+		@Override
+		public View getDropDownView(int position, View convertView, ViewGroup parent) {
+			Typeface myFont = Typeface.createFromAsset(getAssets(), "fonts/Igualb.ttf");
+			TextView v = (TextView) super.getDropDownView(position, convertView, parent);
+			v.setTextColor(Color.RED);
+			v.setTypeface(myFont);
+			return v;
+		}
+
+	}
+
+	private class MySpinnerAdapter2 extends ArrayAdapter {
+
+		public MySpinnerAdapter2(Context context, int textViewResourceId) {
+			super(context, textViewResourceId);
 		}
 
 		@Override
